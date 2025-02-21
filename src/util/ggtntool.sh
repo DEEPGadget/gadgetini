f2779429fff67a84a6b278262d79107aaffa06ea
gksgustmd1 <gksgustmd1@naver.com>
2025-02-21 19:25:42 +0900

    ggtn ip set, displaymode set update

--- src/util/ggtntool.sh
+++ src/util/ggtntool.sh
@@ -6,7 +6,7 @@ print_help() {
     echo "Commands:"
     echo "  --help            Display ggtn command help message"
     echo "  set               Set system configurations"
-    echo "    ip              Set IP address (Server IP, main DNS, serve DNS, ggtn IP, main DNS, serve DNS"
+    echo "    network         Set IP address (dhcp, static"
     echo "    displaymode     Set ggtn display mode, v : vertical, h : horizontal"
     echo "    time            Set system time"
     echo "  get               Get system information"
@@ -18,6 +18,21 @@ print_help() {
     echo "    tt              Import tt dashboard json"
 }
 
+# Subnet Mask to CIDR Conversion
+subnet_to_cidr() {
+    local subnet_mask=$1
+    local -i cidr=0
+    IFS='.' read -ra ADDR <<< "$subnet_mask"
+    for octet in "${ADDR[@]}"; do
+        while [ $octet -gt 0 ]; do
+            cidr=$((cidr + (octet & 1)))  
+            octet=$((octet >> 1))        
+        done
+    done
+    echo $cidr
+}
+
+# Validate IP Address
 validate_ip() {
     local ip=$1
     if [[ $ip =~ ^([0-9]{1,3}\.){3}[0-9]{1,3}$ ]]; then
@@ -35,100 +50,42 @@ validate_ip() {
     fi
 }
 
-#set static server ip, gadgetini ip
-set_ip() {
-    echo "Setting static IP for Server..."
-    echo "Set server IP"
-    read server_ip
-    if ! validate_ip "$server_ip"; then
-        exit 1
+#get current server ip
+network_interface=$(ip link show | awk '$9 == "UP" {print $2}' | sed 's/://g' | head -n 1)
+server_ip_address=$(ip -4 addr show $network_interface | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
+
+set_network() {
+    if [[ $1 == "dhcp" ]]; then
+        echo "Setting DHCP configuration..."
+        /home/deepgadget/gadgetini/src/util/GGTN_set_ip.sh $1 $server_ip_address
+        return
+    else
+        if [[ $# -eq 4 ]] || [[ $# -eq 5 ]]; then
+            local ip_address=$1
+            local subnet_mask=$2
+            local gateway=$3
+            local dns_primary=$4
+            local dns_secondary=${5:-""}  # Optional secondary DNS
+
+            echo "Setting static IP configuration..."
+            if ! validate_ip "$ip_address" || ! validate_ip "$gateway" || ! validate_ip "$dns_primary" || ( [[ -n $dns_secondary ]] && ! validate_ip "$dns_secondary" ); then
+                echo "Validation failed for one or more IP addresses."
+                return 1
+            fi
+
+            local cidr=$(subnet_to_cidr $subnet_mask)
+            echo "IP address set to $ip_address/$cidr"
+            echo "Gateway set to $gateway"
+            echo "Primary DNS set to $dns_primary"
+            [[ -n $dns_secondary ]] && echo "Secondary DNS set to $dns_secondary"
+            echo "current server IP address : $server_ip_address"
+            /home/deepgadget/gadgetini/src/util/GGTN_set_ip.sh "$server_ip_address" "$ip_address/$cidr" "$gateway" "$dns_primary" "$dns_secondary"
+            return
+        else
+            echo "Invalid arguments for setting network."
+            return 1
+        fi
     fi
-    #echo "$server_ip"
-
-    echo "Set server gateway"
-    read server_gateway
-    if ! validate_ip "$server_gateway"; then
-        exit 1
-    fi
-    #echo "$server_gateway"
-
-    echo "set server main dns"
-    read server_primary_dns
-    if ! validate_ip "$server_primary_dns"; then
-        exit 1
-    fi
-    #echo "$server_primary_dns"
-
-    echo "set server serve dns"
-    read server_secondary_dns
-    if ! validate_ip "$server_secondary_dns"; then
-        exit 1
-    fi
-    #echo "$server_secondary_dns"
-
-    #check network interface
-    #echo "Available network interfaces:"
-    #ip link show | grep "BROADCAST,MULTICAST" | cut -d: -f2 | awk '{$1=$1};1'
-    #read INTERFACE
-    #network_interface=$INTERFACE
-    #echo "network interface is : $network_interface"
-
-    # Send server IP information to set_ip.sh
-    #./set_ip.sh $server_ip $server_gateway $server_primary_dns $server_secondary_dns
-
-    echo "Setting static IP for Gadgetini..."
-    echo "Set ggtn IP"
-    read ggtn_ip
-    if ! validate_ip "$ggtn_ip"; then
-        exit 1
-    fi
-
-    echo "Set ggtn gateway"
-    read ggtn_gateway
-    if ! validate_ip "$ggtn_ip"; then
-        exit 1
-    fi
-
-    echo "Set ggtn main dns"
-    read ggtn_primary_dns
-    if ! validate_ip "$ggtn_primary_dns"; then
-        exit 1
-    fi
-
-    echo "Set ggtn serve dns"
-    read ggtn_secondary_dns
-    if ! validate_ip "$ggtn_secondary_dns"; then
-        exit 1
-    fi
-
-    # Output the settings
-    echo "Server static IP: $server_ip"
-    echo "Server Gateway: $server_gateway"
-    echo "Server primary DNS: $server_primary_dns"
-    echo "Server secondary DNS: $server_secondary_dns"
-    echo "--------------------------------"
-    echo "Gadgetini static IP: $ggtn_ip"
-    echo "Gadgetini Gateway: $ggtn_gateway"
-    echo "Gadgetini primary DNS: $ggtn_primary_dns"
-    echo "Gadgetini secondary DNS: $ggtn_secondary_dns"
-    echo "--------------------------------" 
-    echo "IP setting complete...."
-    echo "Please rejoin new IP address please...."
-
-    #ggtntool ip change 
-    #Send ggtn IP information to ggtn set_ip.sh
-    #./set_ip.sh
-    ./GGtn_set_ip.sh $server_ip $ggtn_ip $ggtn_gateway $ggtn_primary_dns $ggtn_secondary_dns
- 
-    #Send server IP information to server set_ip.sh
-    #server ip change
-    #./set_ip.sh $server_ip $server_gateway $server_primary_dns $server_secondary_dns $network_interface 
-
-}
-
-#get Grafana ip
-get_ip() {
-    echo "Getting current IP address..."
 }
 
 #change display mode
@@ -154,15 +111,6 @@ set_displaymode() {
     fi
 }
 
-#set server time, gadgetini time
-set_time() {
-    echo "Setting system time..."
-}
-
-#send json, yaml file to gadgetini
-import_config() {
-    echo "Importing configuration..."
-}
 
 case "$1" in
     --help)
@@ -170,8 +118,9 @@ case "$1" in
         ;;
     set)
         case "$2" in
-            ip)
-                set_ip
+            network)
+                shift 2
+                set_network "$@"
                 ;;
             displaymode)
                 set_displaymode "$3"
@@ -196,8 +145,22 @@ case "$1" in
             ip)
                 get_ip
                 ;;
-            *) 
+            *)
                 echo "Invalid get option: $2"
+                print_help
+                ;;
+        esac
+        ;;
+    import)
+        case "$2" in
+            gpu)
+                import_gpu
+                ;;
+            tt)
+                import_tt
+                ;;
+            *)
+                echo "Invalid import option: $2"
                 print_help
                 ;;
         esac
@@ -205,19 +168,5 @@ case "$1" in
     *)
         echo "Unknown command: $1"
         print_help
-        ;;
-    import)
-        case "$2" in
-            gpu)
-                get_gpu
-                ;;
-            tt)
-                get_tt
-                ;;
-            *)
-                echo "Invalid get option: $2"
-                print_help
-                ;;
-        esac
         ;;
 esac
