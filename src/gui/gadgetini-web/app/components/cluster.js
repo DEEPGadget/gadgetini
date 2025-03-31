d4baf1aa074a292d6df5c42b68be13896fd8175c
theoldtree <yjeongmu@gmail.com>
2025-03-31 18:06:36 +0900

    DEV-50 cluster add page create

--- src/gui/gadgetini-web/app/components/cluster.js
+++ src/gui/gadgetini-web/app/components/cluster.js
@@ -1,16 +1,6 @@
 "use client";
 
 import React, { useState, useEffect, useRef } from "react";
-import {
-  ArrowUpIcon,
-  ArrowRightIcon,
-  CheckIcon,
-  ArrowTopRightOnSquareIcon,
-} from "@heroicons/react/24/solid";
-import LoadingSpinner from "../utils/LoadingSpinner";
-import { getSelfIP } from "../utils/ip/getSelfIP";
-import { getDisplayConfig } from "../utils/display/getDisplayConfig";
-
 export default function Cluster() {
   return <div className="p-4"></div>;
 }
