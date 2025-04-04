4fb1b5c2da5be4efdc107e598238ba3b836f1353
theoldtree <yjeongmu@gmail.com>
2025-04-04 13:44:51 +0900

    definig required function

--- src/gui/gadgetini-web/app/api/nodelist/route.js
+++ src/gui/gadgetini-web/app/api/nodelist/route.js
@@ -1,8 +1,19 @@
 import { NextResponse } from "next/server";
 export async function GET() {
+  //TODO DB에서 ip, alias 정보를 꺼냄
   const nodeList = [
-    { ip: "192.168.1.100", name: "worker" },
-    { ip: "192.168.1.101", name: "worker1" },
+    { ip: "192.168.1.100", status: false, alias: "worker" },
+    { ip: "192.168.1.101", status: false, alias: "worker1" },
   ];
   return NextResponse.json(nodeList);
+}
+
+// TODO DB에서 ip 수정
+export async function PATCH() {
+  return NextResponse.json({ message: "Edit success" });
+}
+
+// TODO DB에 nodetable 업데이트
+export async function PUT() {
+  return NextResponse.json({ message: "Edit success" });
 }
--- src/gui/gadgetini-web/app/components/cluster.js
+++ src/gui/gadgetini-web/app/components/cluster.js
@@ -3,13 +3,53 @@ import React, { useState, useEffect, useRef } from "react";
 import { getNodeList } from "../utils/getNodeList";
 
 export default function Cluster() {
-  const [nodeList, setNodeList] = useState([]);
+  // TODO node add 할때 사용
+  const nodeInputInfo = useRef({
+    num: 1,
+    ip: "",
+    alias: "",
+  });
+  // TODO node table fetch할때 사용용
+  const [nodeTable, setNodeTable] = useState([
+    { ip: "192.168.1.100", status: false, alias: "worker" },
+  ]);
+
+  //TODO node 한개의 status check 하여 상태 return
+  const checkNodeStatus = async (node) => {
+    let status;
+    return {};
+  };
+  //TODO nodeTable에 ip 를 기반으로 node들의 상태 return
+  const checkAllNodeStatus = async (nodeTable) => {
+    nodeTable.map((node) => {
+      checkNodeStatus(node);
+    });
+    setNodeTable(nodeTable);
+  };
+
   useEffect(() => {
-    getNodeList().then(setNodeList);
+    getNodeList().then(setNodeTable);
+    checkNodeStatus();
   }, []);
+
+  // TODO cluster ADD 버튼
+  const handleClusterAdd = async () => {};
+  // TODO nodeTable 에서 node 에 어떤 key를 수정할건지 전달 후 response 처리리
+  const handleEdit = async (node) => {
+    const payload = {
+      ip: node.ip,
+      key: node.key,
+      value: node.value,
+    };
+    const response = await fetch("/api/nodelist", {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(payload),
+    });
+  };
   return (
     <div className="p-4 ">
-      {nodeList.map((node, index) => (
+      {nodeTable.map((node, index) => (
         <div key={index}>{node.ip}</div>
       ))}
     </div>
