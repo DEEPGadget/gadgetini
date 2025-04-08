8d7624fff745ca7e9de236caf34ca138af4e627c
gksgustmd1 <gksgustmd1@naver.com>
2025-04-08 16:51:33 +0900

    new ggtntool..

--- src/util/send_ip.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#get current server ip
-network_interface=$(ip link show | awk '$9 == "UP" {print $2}' | sed 's/://g' | head -n 1)
-server_ip_address=$(ip -4 addr show $network_interface | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
-
-echo "GGTN current IP : $server_ip_address"
-
--- src/util/send_time.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-current_server_time=$(date '+%Y-%m-%d %H:%M:%S')
-
-echo "GGTN current time : $current_server_time"
