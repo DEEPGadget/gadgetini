1983d5d6787584fb81fc962a5e050b916457c5c5
theoldtree <yjeongmu@gmail.com>
2025-11-12 13:50:26 +0900

    gadgetini icon added

--- src/gui/grafana-config.sh
+++ src/gui/grafana-config.sh
@@ -4,7 +4,11 @@ cd /usr/share/grafana/public/build
 sudo sed -i 's|B\.M\.AppTitle|"gadgetini"|g' 3729.*.js
 sudo sed -i 's|To\.M\.AppTitle|"gadgetini"|g' 3729.*.js
 sudo sed -i 's/" - "/" | "/g' 3729.*.js
-sudo systemctl restart grafana-server
+
+sudo cp fav32.png /usr/share/grafana/public/img/fav32.png
+sudo cp grafana_icon.svg /usr/share/grafana/public/img/grafana_icon.svg
+
 sudo sed -i 's/;theme = dark/theme = light/g' /etc/grafana/grafana.ini
 sudo sed -i 's/theme = dark/theme = light/g' /etc/grafana/grafana.ini
 
+sudo systemctl restart grafana-server
