96a29524eb29b3d136994c414f480bdd8191494a
theoldtree <yjeongmu@gmail.com>
2025-11-13 14:36:38 +0900

    grafana config updated

--- src/gui/grafana-config.sh
+++ src/gui/grafana-config.sh
@@ -5,10 +5,12 @@ sudo sed -i 's|B\.M\.AppTitle|"gadgetini"|g' 3729.*.js
 sudo sed -i 's|To\.M\.AppTitle|"gadgetini"|g' 3729.*.js
 sudo sed -i 's/" - "/" | "/g' 3729.*.js
 
-sudo cp fav32.png /usr/share/grafana/public/img/fav32.png
-sudo cp grafana_icon.svg /usr/share/grafana/public/img/grafana_icon.svg
+cd /home/gadgetini/gadgetini/src/gui
+sudo cp ./fav32.png /usr/share/grafana/public/img/fav32.png
+sudo cp ./grafana_icon.svg /usr/share/grafana/public/img/grafana_icon.svg
 
-sudo sed -i 's/;theme = dark/theme = light/g' /etc/grafana/grafana.ini
-sudo sed -i 's/theme = dark/theme = light/g' /etc/grafana/grafana.ini
+sudo sed -i 's/;defalut_theme = dark/default_theme = light/g' /etc/grafana/grafana.ini
+sudo sed -i 's/default_theme = dark/default_theme = light/g' /etc/grafana/grafana.ini
+sudo sed -i 's/;default_theme = light/default_theme = light/g' /etc/grafana/grafana.ini
 
 sudo systemctl restart grafana-server
