0873083e542d92920466beed681cfe1d48d634fe
theoldtree <yjeongmu@gmail.com>
2025-12-05 11:53:39 +0900

    gadgetini shell script file error resolved & usb-gadget-gadgetini-network.sh added

--- src/configure/usb-gadget-gadgetini.sh
+++ src/configure/usb-gadget-gadgetini.sh
@@ -8,13 +8,10 @@ CMDLINE_FILE="/boot/firmware/cmdline.txt"
 CONFIG_FILE="/boot/firmware/config.txt"
 SERVICE_FILE="/etc/systemd/system/usb-gadget-up.service"
 
-CMDLINE_CONTENT="overlayroot=tmpfs console=serial0,9600 root=PARTUUID=1e7ee8d2-02 rootfstype=ext4 fsck.repair=yes rootwait quiet splash plymouth.ignore-serial-consoles cfg80211.ieee80211_regdom=GB modules-load=dwc2,g_ether g_ether.dev_addr=${GADGET_MAC}"
 
 # --- Overwrite cmdline.txt ---
-sudo tee "${CMDLINE_FILE}" > /dev/null <<EOF
-${CMDLINE_CONTENT}
-EOF
-echo "1. cmdline overwritten"
+sudo sed -i 's/$/ modules-load=dwc2,g_ether g_ether.dev_addr=fe:f1:1a:d3:6e:b6/' /boot/firmware/cmdline.txt
+echo "1. ${CMLINE_FILE} modified"
 
 # --- Overwrite config.txt ---
 sudo tee "${CONFIG_FILE}" > /dev/null <<EOF
@@ -49,33 +46,7 @@ enable_uart=1
 dtoverlay=disable-bt
 dtoverlay=dwc2
 EOF
-echo "2. config overwritten"
+echo "2. ${CONFIG_FILE} overwritten"
 
-# --- NetworkManager connection profile ---
-sudo nmcli connection delete "${NM_CON_NAME}" 2>/dev/null || true
-sudo nmcli connection add type ethernet ifname usb0 con-name "${NM_CON_NAME}" \
-    ipv4.method manual ipv4.addresses "${STATIC_IP}" connection.autoconnect yes
-sudo nmcli connection modify "${NM_CON_NAME}" ipv4.route-metric 700
-echo "3. NM profile created (${NM_CON_NAME})"
-
-# --- Systemd service creation ---
-sudo tee "${SERVICE_FILE}" > /dev/null <<EOF
-[Unit]
-Description=Activate Gadget USB Network Connection
-After=network-online.target
-
-[Service]
-ExecStart=/usr/bin/nmcli connection up ${NM_CON_NAME} ifname usb0
-Type=oneshot
-RemainAfterExit=yes
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
-sudo systemctl daemon-reload
-sudo systemctl enable "usb-gadget-up.service"
-echo "4. systemd service enabled (usb-gadget-up.service)"
-
-echo "5. Gadget configuration complete (reboot required)"
+echo "3. Gadget configuration complete (reboot required)"
 
