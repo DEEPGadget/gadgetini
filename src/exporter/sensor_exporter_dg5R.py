03e01c82e93142d80d5a4220af1f2fe2c6010d0d
theoldtree <yjeongmu@gmail.com>
2026-02-19 14:44:55 +0900

    add redis key value to senor_exporter_dg5R

--- src/exporter/sensor_exporter_dg5R.py
+++ src/exporter/sensor_exporter_dg5R.py
@@ -7,6 +7,113 @@ import redis
 
 client = redis.StrictRedis(host='localhost', port=6379, db=0, decode_responses=True)
 
+# Redis KEYS *  (grouped + expected unit)
+""" 
+[GPU - name]
+- gpu_name_0                (string)   GPU model
+- gpu_name_1                (string)
+- gpu_name_2                (string)
+- gpu_name_3                (string)
+- gpu_name_4                (string)
+- gpu_name_5                (string)
+- gpu_name_6                (string)
+- gpu_name_7                (string)
+
+[GPU - temp]
+- gpu_temp_0                (°C)       GPU temp
+- gpu_temp_1                (°C)
+- gpu_temp_2                (°C)
+- gpu_temp_3                (°C)
+- gpu_temp_4                (°C)
+- gpu_temp_5                (°C)
+- gpu_temp_6                (°C)
+- gpu_temp_7                (°C)
+
+[GPU - current power]
+- gpu_curr_pwr_0            (W)        GPU current power consumption
+- gpu_curr_pwr_1            (W)
+- gpu_curr_pwr_2            (W)
+- gpu_curr_pwr_3            (W)
+- gpu_curr_pwr_4            (W)
+- gpu_curr_pwr_5            (W)
+- gpu_curr_pwr_6            (W)
+- gpu_curr_pwr_7            (W)
+
+[GPU - max power]
+- gpu_max_pwr_0             (W)        GPU power limit
+- gpu_max_pwr_1             (W)
+- gpu_max_pwr_2             (W)
+- gpu_max_pwr_3             (W)
+- gpu_max_pwr_4             (W)
+- gpu_max_pwr_5             (W)
+- gpu_max_pwr_6             (W)
+- gpu_max_pwr_7             (W)
+
+[GPU - current mem]
+- gpu_curr_mem_0            (MiB)      GPU vram currently used 
+- gpu_curr_mem_1            (MiB)
+- gpu_curr_mem_2            (MiB)
+- gpu_curr_mem_3            (MiB)
+- gpu_curr_mem_4            (MiB)
+- gpu_curr_mem_5            (MiB)
+- gpu_curr_mem_6            (MiB)
+- gpu_curr_mem_7            (MiB)
+
+[GPU - max mem]
+- gpu_max_mem_0             (MiB)      GPU vram total size
+- gpu_max_mem_1             (MiB)
+- gpu_max_mem_2             (MiB)
+- gpu_max_mem_3             (MiB)
+- gpu_max_mem_4             (MiB)
+- gpu_max_mem_5             (MiB)
+- gpu_max_mem_6             (MiB)
+- gpu_max_mem_7             (MiB)
+
+[CPU]
+- cpu_usage                 (%)        CPU usage
+- cpu_temp_0                (°C)       CPU package temperature (socket0)
+- cpu_temp_1                (°C)       CPU package temperature (socket1)
+- cpu_curr_pwr_0                (W)        CPU0 power consumption (package/socket power)
+- cpu_curr_pwr_1                (W)        CPU1 power consumption
+
+[Memory]
+- mem_total                 (GiB)    total memory 
+- mem_available             (GiB)    available memory
+- mem_usage                 (%)      memory usage
+
+
+[NIC]
+- nic_ens102f0_stat         (string/boolean(true/false))  link status (UP/DOWN)
+- nic_ens102f1_stat         (string/boolean)
+- nic_enP1s125f0np0_stat    (string/boolean)
+- nic_enP1s125f1np1_stat    (string/boolean)
+- nic_enxfef11ad36eb7_stat  (string/boolean)
+- nic_enxee7c7c859844_stat  (string/boolean)
+
+
+[Infiniband / IB]
+- ib_nic_temp               (°C)       IB NIC 칩셋 온도
+
+[Cooling - coolant]
+- coolant_temp_inlet1       (°C)       coolant inlet temperature (channel1)
+- coolant_temp_inlet2       (°C)       coolant inlet temperature (channel2)
+- coolant_temp_outlet1      (°C)       coolant outlet temperature (channel1)
+- coolant_temp_outlet2      (°C)       coolant outlet temperature (channel2)
+- coolant_delta_t1          (°C)       ΔT = outlet1 - inlet1
+- coolant_delta_t2          (°C)       ΔT = outlet2 - inlet2
+- coolant_level             (0/1 bool) coolant level (0 Low, 1 High) 
+- coolant_leak              (0/1 bool) coolant leakage detection (0 Noraml, 1 Leak detected) 
+
+[Air / Environment]
+- air_temp                  (°C)       internal air temperature
+- air_humit                 (%RH)      internal air relative humidity
+
+[Host]
+- host_stat (0/1 bool) host server status check (0: offline, 1 online)
+
+"""
+
+
 def get_float(key, default=0.0):
     try:
         v = client.get(key)
