a90defce022d1cf02ae0865b812279383e8b9ca7
theoldtree <yjeongmu@gmail.com>
2026-02-24 10:08:56 +0900

    modify grafana default config

--- src/gui/grafana/grafana-config.sh
+++ src/gui/grafana/grafana-config.sh
@@ -5,6 +5,11 @@ sudo sed -i 's|B\.M\.AppTitle|"gadgetini"|g' 3729.*.js
 sudo sed -i 's|To\.M\.AppTitle|"gadgetini"|g' 3729.*.js
 sudo sed -i 's/" - "/" | "/g' 3729.*.js
 
+sudo sed -i 's/static{this\.AppTitle="Grafana"}/static{this.AppTitle="gadgetini"}/g' 3729.*.js
+sudo sed -i 's/static{this\.LoginTitle="Welcome to Grafana"}/static{this.LoginTitle="Welcome to gadgetini"}/g' 3729.*.js
+sudo sed -i 's/{text:"Grafana"}/{text:"gadgetini"}/g' 3729.*.js
+
+
 cd /home/gadgetini/gadgetini/src/gui
 sudo cp ./fav32.png /usr/share/grafana/public/img/fav32.png
 sudo cp ./grafana_icon.svg /usr/share/grafana/public/img/grafana_icon.svg
@@ -12,5 +17,7 @@ sudo cp ./grafana_icon.svg /usr/share/grafana/public/img/grafana_icon.svg
 sudo sed -i 's/;defalut_theme = dark/default_theme = light/g' /etc/grafana/grafana.ini
 sudo sed -i 's/default_theme = dark/default_theme = light/g' /etc/grafana/grafana.ini
 sudo sed -i 's/;default_theme = light/default_theme = light/g' /etc/grafana/grafana.ini
+
+sudo grafana-cli plugins install gapit-htmlgraphics-panel
 
 sudo systemctl restart grafana-server
